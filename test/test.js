2c1cfdb470cf617e58b6203b1da92b0d58168c5e
Giancarlo Corzo <gian.corzo@gmail.com>
2017-03-06 10:32:27 -0500

    Creando el html y el file de testing

---
+++ test/test.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+var app    = require('../assets/js/app.js');
+
+describe("Tictactoe app test",function() {
+
+  it("Poder iniciar un nuevo juego", function() {
+
+  });
+
+  it("Un jugador X debe poder hacer su jugada",function() {
+
+  });
+
+  it("Validar que algún jugador ganó el juego",function() {
+
+  });
+
+  it("Valida que hay empate",function() {
+
+  });
+});
